491537b2ed8c7340a01a6a5f2e62b09e37289bbc
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2019-06-28 20:27:04 -0500

    working on state/lifecyle

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import Header from "./Header";
 import GameBoard from "./GameBoard";
-import * as serviceWorker from "./serviceWorker";
+// import * as serviceWorker from "./serviceWorker";
 
 class Game extends React.Component {
   render() {
@@ -25,19 +25,54 @@ class Game extends React.Component {
 //===================================================
 //TESTING
 
-function Welcome(props) {
-  return <h1>Hell0, {props.name + " " + props.last}</h1>;
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: new Date()
+    };
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(() => this.tick(), 1000);
+  }
+
+  //WARNING! To be deprecated in React v17. Use componentDidMount instead.
+  // componentWillMount() {
+  //   clearInterval(this.timerID);
+  // }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, World!</h1>
+        <h2>It Is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
 }
 
-function App2() {
-  return (
-    <div>
-      <Welcome name="Nate" last="Daniels" />
-    </div>
-  );
-}
+ReactDOM.render(<Clock />, document.getElementById("root2"));
 
-ReactDOM.render(App2(), document.getElementById("root2"));
+// function Welcome(props) {
+//   return <h1>Hello, {props.name + " " + props.last}</h1>;
+// }
+
+// function App2() {
+//   return (
+//     <div>
+//       <Welcome name="Nate" last="Daniels" />
+//     </div>
+//   );
+// }
+
+// ReactDOM.render(App2(), document.getElementById("root2"));
 
 // END TESTING
 // ========================================
@@ -47,4 +82,4 @@ ReactDOM.render(<Game />, document.getElementById("root"));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
